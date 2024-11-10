06609880aaddef28569851337783acfa2911a81d
dylan12manley <dylan.manley@gmail.com>
2024-11-10 15:21:07 -0800

    add id row to table

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json())
 
 app.post('/posts', async (req, res) => {
-  const {title, content, category = null} = req.body;
+  const {id, title, content, category = null} = req.body;
 
-  if (!title || !content) {
+  if (!id || !title || !content) {
     return res.status(400).send('please send title and content');
   }
 
   const [result] = await db.execute(
-    "INSERT INTO posts (title, content, category) VALUES (?,?,?)",
-    [title, content, category]
+    "INSERT INTO posts (id, title, content, category) VALUES (?,?,?,?)",
+    [id, title, content, category]
   );
   res.send({ sucess: result.affectedRows > 0 });
 });
