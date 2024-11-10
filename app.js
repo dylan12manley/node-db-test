9c709296b411c622cde8444e698505b1e86ec871
dylan12manley <dylan.manley@gmail.com>
2024-11-10 14:55:12 -0800

    reinitialised repo

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.post('/posts', async (req, res) => {
+  const {title, content, category} = req.body;
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
